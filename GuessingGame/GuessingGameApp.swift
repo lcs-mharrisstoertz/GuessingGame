9ffa4de072ac91b61d35a321d82ff1efab7a877c
Morgan <Mharrisstoertz@gmail.com>
2023-02-01 10:14:05 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Morgan Harris-Stoertz on 2023-02-01.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
